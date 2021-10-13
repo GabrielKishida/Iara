aec860504745cad94aa2d942e0284562bf82e401
Rodrigo Tei <rodrigo.tei@inspira.com.br>
2021-10-13 13:10:53 -0300

    adiciona card de texto com imagem

---
+++ src/components/TextCard/TextCard.tsx
@@ -0,0 +1,29 @@
+import { CardContainer, ImageCardContainer, ImageRow } from "./TextCard.style";
+import { Col, Row } from "../theme/grid";
+import { H4, H5 } from "../typography";
+import TextCardMock from "./TextCardMock";
+import { COLORS } from "../theme";
+
+export const TextCard: React.FC = () => {
+  return (
+    <CardContainer inputColor={COLORS.xLightGray}>
+      <div>
+        <H4> {TextCardMock.title} </H4>
+      </div>
+      <ImageRow>
+        <Col>
+          <ImageCardContainer inputColor={COLORS.verdeEscuro}>
+            <img src={TextCardMock.image}/>
+          </ImageCardContainer>
+        </Col>
+      </ImageRow>
+      <Row>
+        <Col>
+            <H5>
+                {TextCardMock.text}
+            </H5>
+        </Col>
+      </Row>  
+    </CardContainer>
+  );
+};
