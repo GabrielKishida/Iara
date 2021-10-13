0e229be4af04e8f555e0453039b1b0c8398d262b
Rodrigo Tei <rodrigo.tei@inspira.com.br>
2021-10-13 10:44:13 -0300

    adiciona elementos do card

---
+++ src/components/QuestionCard/QuestionCard.style.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { COLORS, SPACING } from "../theme";
+import { Col, Row } from "../theme/grid";
+
+interface CardContainerProps {
+    inputColor: string; 
+  }
+
+export const CardContainer = styled.div<CardContainerProps>`
+  class: "flex-container";
+  background-color: ${({inputColor}) => inputColor};
+  border-radius: ${SPACING.borderRadius}px;
+  padding: ${SPACING.medium}px;
+  flex-direction: row;
+  max-width: 50%;
+  flex: 0 0 auto;
+  justify-content: center;
+  padding: 150px, 150px;
+`;
+
+export const ImageCardContainer = styled(CardContainer)`
+    margin: 30px 0px;
+    max-width: 100%;
+`
+
+export const ImageRow = styled(Row)`
+    justify-content: center;
+`
+
+export const AnswerButton = styled.button`
+  class: "flex-container";
+  background-color: ${COLORS.verdeEscuro};
+  border-radius: ${SPACING.borderRadius}px;
+  padding: ${SPACING.medium}px;
+  flex-direction: row;
+  min-width: 100px;
+`;
+
+export const AnswerRow = styled(Row)`
+  justify-content: space-evenly;
+  margin: 15px 0px;
+`;
---
+++ src/components/QuestionCard/QuestionCardMock.js
@@ -0,0 +1,13 @@
+const QuestionCardMock = {
+    title: 'Qual escala é essa?',
+    image: 'https://i.ytimg.com/vi/uH_jOR_p1N8/hqdefault.jpg',
+    alternatives: [
+        'G',
+        'Am',
+        'Em',
+        'Cm',
+        'E',
+        'F'
+    ]
+  }
+  export default QuestionCardMock
