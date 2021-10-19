9ec5f18077baa33ffc7a9bc827656167818ac896
GabrielKishida <gabriel.kishida@usp.br>
2021-10-19 16:18:31 -0300

    🔧 Add Cypress

---
+++ cypress/integration/iara-tests/sample_spec.js
@@ -0,0 +1,8 @@
+describe("My First Test", () => {
+  it('finds the content "type"', () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.contains("Usuário").click();
+    cy.url().should("include", "/user");
+  });
+});
