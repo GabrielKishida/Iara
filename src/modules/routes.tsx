dfde2d9bef5e9776952af4b892268dafc1e33f2d
GabrielKishida <gabriel.kishida@usp.br>
2021-10-19 00:14:34 -0300

    ✨ Add classes pages

--- src/modules/routes.tsx
+++ src/modules/routes.tsx
@@ -3,6 +3,8 @@ import { CoursePage } from "./CoursePage/CoursePage";
 import { UserPage } from "./UserPage/UserPage";
 
 import { BrowserRouter, Route } from "react-router-dom";
+import { FinishedClassesPage } from "./ClassesPage/FinishedClassesPage";
+import { InProgressClassesPage } from "./ClassesPage/InProgressClassesPage";
 
 const Routes: React.FC = (props) => {
   return (
@@ -11,6 +13,12 @@ const Routes: React.FC = (props) => {
       <Route path="/" exact component={MainPage} />
       <Route path="/course" exact component={CoursePage} />
       <Route path="/user" exact component={UserPage} />
+      <Route path="/classes/finished" exact component={FinishedClassesPage} />
+      <Route
+        path="/classes/in-progress"
+        exact
+        component={InProgressClassesPage}
+      />
     </BrowserRouter>
   );
 };
